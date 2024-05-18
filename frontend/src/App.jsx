77616e8210bad89de73b4ee76bfd841dae35b68d
Amyat103 <zhoudavid7312@gmail.com>
2024-05-17 21:37:35 -0400

    change backend to python

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [array, setArray] = useState([])
+
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8080/api/users")
+    console.log(response.data.users)
+  }
+
+  // run once
+  useEffect(() => {
+    fetchAPI()
+  },[])
 
   return (
     <>
