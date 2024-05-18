ab8e98bb1d1b2c147ed8b3f6e3672b3f968ded65
Amyat103 <zhoudavid7312@gmail.com>
2024-05-17 22:30:22 -0400

    grid sorted news

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,29 +27,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='head-bar'>
+        <h2>News</h2>
       </div>
-      <h1>News</h1>
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        <div>
-          {
-            newspapers.map((newspaper, index) =>(
-              <div key={index}>
-                <h2>{newspaper.title}</h2>
-                <p>{newspaper.state}</p>
-              </div>
-            ))
-          }
-        </div>
+      <div className='grid-container'>
+        {newspapers.map((newspaper, index) =>(
+          <div key={index} className='grid-item'>
+            <h4>{newspaper.title}</h4>
+            <p>{newspaper.lccn}</p>
+            <p>{newspaper.state}</p>
+            <p>{newspaper.url}</p>
+          </div>
+        ))}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
