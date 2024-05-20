c5d2f9cb78346923c8f3044ef2d4fd46fe562297
ngimc1 <115123441+ngimc1@users.noreply.github.com>
2024-05-20 10:55:06 -0400

    added header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
+import Header from './header/header'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <p>
         hellow
       </p>
---
+++ frontend/src/header/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 className="title">
+                stateline news
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
