8e0259e8a2285d7459eae25a9fa7fbd9b0976e47
ngimc1 <115123441+ngimc1@users.noreply.github.com>
2024-05-22 16:46:16 -0400

    created loading screen so fetch data properly, rendered table properly (no style)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,36 +9,43 @@ import BasicTable from './table/table'
 
 function App() {
   
-  
+  //useState to store fetched data
   const [news, setData] = useState([])
 
+  const [loading, setLoading] = useState(true);
+
+  //originally did the .then() way but decided to do this
     useEffect(() => {
       const fetchData = async () => {
         try {
           const res = await fetch('http://localhost:5174/news');
           const data = await res.json();
           setData(data)
+          setLoading(false);
           console.log("fetched data")
         } catch (error) {
           console.log("Error: ", error)
+          setLoading(false);
         }
       };
       fetchData();
     }, [])
 
+  //ANNOYING ISSUE
+  //set a loading page so that api can be fetched properly
+  //before accessing the json data
+  //so that it doesn't run into error 
+  if (loading) {
+    return <div>Loading...</div>
+  }
 
   return (
 
   
     <>
       <Header />
-      <p>
-          {news.newspapers && news.newspapers.length > 0 
-            ? news.newspapers[0].state 
-            : "Loading..."}
-      </p>
       <Filter />
-      <BasicTable />
+      <BasicTable news={news}/>
     </>
   )
 }
--- frontend/src/table/table.jsx
+++ frontend/src/table/table.jsx
@@ -8,51 +8,51 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import "./table.css"
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0),
-  createData('Ice cream sandwich', 237, 9.0),
-  createData('Eclair', 262, 16.0),
-  createData('Cupcake', 305, 3.7),
-  createData('Gingerbread', 356, 16.0)
-];
+export default function BasicTable({ news }) {
 
-export default function BasicTable() {
+
+  const newsData = news.newspapers;
+
+
   return (
-    <TableContainer component={Paper}>
+    <table>
+      <thread>
+        <tr>
+          <th>state</th>
+          <th>title</th>
+          <th>lccn</th>
+        </tr>
+      </thread>
+      <tbody>
+      {newsData.map(news => (
+        <tr key={news.id}>
+          <td>{news.state}</td>
+          <td>{news.title}</td>
+          <td>{news.lccn}</td>
+        </tr>
+      ))}
+    </tbody>
+    </table>
+    
+    // <TableContainer component={Paper}>
 
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    //   <Table sx={{ minWidth: 650 }} aria-label="simple table">
 
-        <TableHead>
-          <TableRow className='legends'>       
-            <TableCell>title</TableCell>
-            <TableCell align="right">state</TableCell>
-            <TableCell align="right">lccn</TableCell>
-          </TableRow>
-        </TableHead>
+    //     <TableHead>
+    //       <TableRow className='legends'>       
+    //         <TableCell>title</TableCell>
+    //         <TableCell align="center">state</TableCell>
+    //         <TableCell align="center">lccn</TableCell>
+    //       </TableRow>
+    //     </TableHead>
         
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        
-      </Table>
+    //     <TableBody>
 
-    </TableContainer>
+    //     </TableBody>
+        
+    //   </Table>
+
+    // </TableContainer>
   );
 }
