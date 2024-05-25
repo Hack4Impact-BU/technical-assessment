1b7ae3461bee80ea9090e2422803826b7158ef1b
Amyat103 <zhoudavid7312@gmail.com>
2024-05-25 17:48:43 -0400

    add css and grid for community

--- frontend/src/Community.jsx
+++ frontend/src/Community.jsx
@@ -6,7 +6,14 @@ function Community() {
       <div className='head-bar'>
         <h2>Community</h2>
       </div>
-      
+      <div className='community-grid'>
+        <div className='sort-grid'>
+          <p>First Name</p>
+          <p>Last Name</p>
+          <p>Date</p>
+        </div>
+        <div className='sort-item'></div>
+      </div>
     </>
   );
 }
