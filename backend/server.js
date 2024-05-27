ed176ef82d0861353e3d867893a0bdf45f10bd0a
jung <eugenejung04@gmail.com>
2024-05-27 01:15:57 -0400

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,27 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const apiRoutes = require('./routes/api');
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001; // Change port
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173'  // Allow requests from this origin
+}));
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/communityDB', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+const mongoURI = process.env.MONGO_URI;
+if (!mongoURI) {
+    console.error('MONGO_URI is not defined in .env');
+    process.exit(1);
+  }
+  
+  mongoose.connect(mongoURI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
 // Routes
 app.use('/api', apiRoutes);
