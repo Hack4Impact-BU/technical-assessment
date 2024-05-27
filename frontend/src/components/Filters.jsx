f05cbcfca807e4d3857c3d554b606707a1dad139
jung <eugenejung04@gmail.com>
2024-05-27 01:25:39 -0400

    add filter options

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -1,5 +1,58 @@
 import React from 'react';
-import { TextField, Button } from '@mui/material';
+import { TextField, Button, MenuItem } from '@mui/material';
+
+const states = [
+  { code: 'AL', name: 'Alabama' },
+  { code: 'AK', name: 'Alaska' },
+  { code: 'AZ', name: 'Arizona' },
+  { code: 'AR', name: 'Arkansas' },
+  { code: 'CA', name: 'California' },
+  { code: 'CO', name: 'Colorado' },
+  { code: 'CT', name: 'Connecticut' },
+  { code: 'DE', name: 'Delaware' },
+  { code: 'FL', name: 'Florida' },
+  { code: 'GA', name: 'Georgia' },
+  { code: 'HI', name: 'Hawaii' },
+  { code: 'ID', name: 'Idaho' },
+  { code: 'IL', name: 'Illinois' },
+  { code: 'IN', name: 'Indiana' },
+  { code: 'IA', name: 'Iowa' },
+  { code: 'KS', name: 'Kansas' },
+  { code: 'KY', name: 'Kentucky' },
+  { code: 'LA', name: 'Louisiana' },
+  { code: 'ME', name: 'Maine' },
+  { code: 'MD', name: 'Maryland' },
+  { code: 'MA', name: 'Massachusetts' },
+  { code: 'MI', name: 'Michigan' },
+  { code: 'MN', name: 'Minnesota' },
+  { code: 'MS', name: 'Mississippi' },
+  { code: 'MO', name: 'Missouri' },
+  { code: 'MT', name: 'Montana' },
+  { code: 'NE', name: 'Nebraska' },
+  { code: 'NV', name: 'Nevada' },
+  { code: 'NH', name: 'New Hampshire' },
+  { code: 'NJ', name: 'New Jersey' },
+  { code: 'NM', name: 'New Mexico' },
+  { code: 'NY', name: 'New York' },
+  { code: 'NC', name: 'North Carolina' },
+  { code: 'ND', name: 'North Dakota' },
+  { code: 'OH', name: 'Ohio' },
+  { code: 'OK', name: 'Oklahoma' },
+  { code: 'OR', name: 'Oregon' },
+  { code: 'PA', name: 'Pennsylvania' },
+  { code: 'RI', name: 'Rhode Island' },
+  { code: 'SC', name: 'South Carolina' },
+  { code: 'SD', name: 'South Dakota' },
+  { code: 'TN', name: 'Tennessee' },
+  { code: 'TX', name: 'Texas' },
+  { code: 'UT', name: 'Utah' },
+  { code: 'VT', name: 'Vermont' },
+  { code: 'VA', name: 'Virginia' },
+  { code: 'WA', name: 'Washington' },
+  { code: 'WV', name: 'West Virginia' },
+  { code: 'WI', name: 'Wisconsin' },
+  { code: 'WY', name: 'Wyoming' }
+];
 
 function Filters({ state, setState, lccn, setLccn, fetchNews }) {
   return (
@@ -9,26 +62,30 @@ function Filters({ state, setState, lccn, setLccn, fetchNews }) {
         value={state}
         onChange={e => setState(e.target.value)}
         select
-        SelectProps={{ native: true }}
+        fullWidth
+        variant="outlined"
+        margin="normal"
       >
-        <option value=""></option>
-        <option value="CA">California</option>
-        <option value="NY">New York</option>
-        {/* Add more state options here */}
+        <MenuItem value="">
+          <em>All States</em>
+        </MenuItem>
+        {states.map((state) => (
+          <MenuItem key={state.code} value={state.code}>
+            {state.name}
+          </MenuItem>
+        ))}
       </TextField>
       <TextField
         label="LCCN"
         value={lccn}
         onChange={e => setLccn(e.target.value)}
-        select
-        SelectProps={{ native: true }}
-      >
-        <option value=""></option>
-        <option value="sn83030214">sn83030214</option>
-        <option value="sn83030215">sn83030215</option>
-        {/* Add more LCCN options here */}
-      </TextField>
-      <Button onClick={fetchNews}>Filter</Button>
+        fullWidth
+        variant="outlined"
+        margin="normal"
+      />
+      <Button variant="contained" color="primary" onClick={fetchNews}>
+        Filter
+      </Button>
     </div>
   );
 }
